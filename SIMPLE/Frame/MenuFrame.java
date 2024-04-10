e63257b101586d298263b19435432ec36b5bf3f1
KangJeongTaek <dfgddjd@naver.com>
2024-04-11 00:17:08 +0900

    Update MenuFrame.java

--- SIMPLE/Frame/MenuFrame.java
+++ SIMPLE/Frame/MenuFrame.java
@@ -7,6 +7,7 @@ import java.awt.GridLayout;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
@@ -30,6 +31,7 @@ public class MenuFrame extends JFrame{
         JPanel eastJPanel = new JPanel();
         JPanel southJPanel = new JPanel();
         JPanel centerJPanel = new JPanel();
+        eastJPanel.setLayout(new BorderLayout());
         northJPanel.setLayout(new GridLayout());
         southJPanel.setLayout(new FlowLayout());
         centerJPanel.setLayout(new GridLayout(3, 3,10,10));
@@ -50,15 +52,39 @@ public class MenuFrame extends JFrame{
         northJPanel.add(beverageJButton);
 
 
-        //우측 패널에 장바구니 만들기
- 
-        
+        //우측 패널 사이즈 조절하기
+        eastJPanel.setPreferredSize(new Dimension(200, getHeight()));
+        //장바구니 이름 표시
+        eastJPanel.add(new JLabel("구매 목록",JLabel.CENTER),BorderLayout.NORTH);
+
+        //장바구니 만들기
+        DefaultTableModel cartTableModel = new DefaultTableModel();
+        cartTableModel.addColumn("번호");
+        cartTableModel.addColumn("제품명");
+        cartTableModel.addColumn("가격");
+        JTable cartTable = new JTable(cartTableModel);
+        JScrollPane sc = new JScrollPane(cartTable);
+        eastJPanel.add(sc);
 
         //남쪽 패널에 결제 버튼 생성하기
         JButton purchaseBJButton = new JButton("구매하기");
         southJPanel.add(purchaseBJButton);
 
+
+        // 가운데 패널에 생성하기
+        JPanel[] prodListJPanels = new JPanel[9];
+
+        for (int i = 0; i<prodListJPanels.length ; i ++){
+            prodListJPanels[i] = new JPanel();
+            prodListJPanels[i].add(new JButton("하이"));
+            centerJPanel.add(prodListJPanels[i]);
+        }
+
         // 나타나게 설정
         setVisible(true);
     }
+}
+
+class RamenJPanel extends JPanel{
+
 }
