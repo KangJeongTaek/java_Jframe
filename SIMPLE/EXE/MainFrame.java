51a4ef61a36000048e3f565d6eb9f25d68343162
KangJeongTaek <dfgddjd@naver.com>
2024-04-19 09:12:56 +0900

    Update MainFrame.java

--- SIMPLE/EXE/MainFrame.java
+++ SIMPLE/EXE/MainFrame.java
@@ -104,7 +104,6 @@ public class MainFrame extends JFrame{
                     if(udb.logincheck(id,pas)){
                         udb.databaseClose();
                         dispose();
-                        MainFrame.getInstance().dispose();
                         new LoginAfterFrame();
                     }else{
                         JOptionPane.showMessageDialog(null, "해당하는 정보가 없습니다. 아이디/ 비밀번호를 확인해주세요.");
